5fff217fc55b46cb239b43ec6fde8c900e0de794
Mike Bros <mbros@roofmaxx.com>
2021-10-26 12:36:43 -0500

    combination seeder is now working as expected and will dynamically create rows for each individual scent to account for none combination fragrances

--- database/seeders/CombinationSeeder.php
+++ database/seeders/CombinationSeeder.php
@@ -42,10 +42,10 @@ class CombinationSeeder extends Seeder
                 'amount_1' => .6,
                 'scent_id_2' => 3,
                 'amount_2' => .5,
-            ),
+            )
         );
         $this->insert($comb_list);
-
+        $this->createSingles();
 //        foreach (Scent::all()->where('company','Candle Science') as $scent){
 //            $combination = new Combination();
 //            $combination->scent_id_1 = $scent->scent_id;
@@ -56,43 +56,40 @@ class CombinationSeeder extends Seeder
 
     private function insert(array $items)
     {
-        /*foreach ($items as $item) {
-            if (isset($item['scent_id_4'])) {
-                $combination = new Combination();
-                $combination->scent_id_1 = $item['scent_id_1'];
-                $combination->amount_1 = $item['amount_1'];
-                $combination->scent_id_2 = $item['scent_id_2'];
-                $combination->amount_2 = $item['amount_2'];
-                $combination->scent_id_3 = $item['scent_id_3'];
-                $combination->amount_3 = $item['amount_3'];
-                $combination->scent_id_4 = $item['scent_id_4'];
-                $combination->amount_4 = $item['amount_4'];
-                $combination->save();
-
-            } elseif (isset($item['scent_id_3'])) {
-                $combination = new Combination();
-                $combination->scent_id_1 = $item['scent_id_1'];
-                $combination->amount_1 = $item['amount_1'];
-                $combination->scent_id_2 = $item['scent_id_2'];
-                $combination->amount_2 = $item['amount_2'];
-                $combination->scent_id_3 = $item['scent_id_3'];
-                $combination->amount_3 = $item['amount_3'];
-                $combination->save();
-
-            } elseif (isset($item['scent_id_2'])) {
-                $combination = new Combination();
-                $combination->scent_id_1 = $item['scent_id_1'];
-                $combination->amount_1 = $item['amount_1'];
-                $combination->scent_id_2 = $item['scent_id_2'];
-                $combination->amount_2 = $item['amount_2'];
-                $combination->save();
-
-            } else {
-                $combination = new Combination();
-                $combination->scent_id_1 = $item['scent_id_1'];
-                $combination->amount_1 = $item['amount_1'];
-                $combination->save();
-            }
-        }*/
+        foreach ($items as $item) {
+            Combination::updateOrCreate([
+                'scent_id_1' => $item['scent_id_1'],
+                'amount_1' => $item['amount_1'],
+                'scent_id_2' => ($item['scent_id_2'] ?? null),
+                'amount_2' => ($item['amount_2'] ?? null),
+                'scent_id_3' => ($item['scent_id_3'] ?? null),
+                'amount_3' => ($item['amount_3'] ?? null),
+                'scent_id_4' => ($item['scent_id_4'] ?? null),
+                'amount_4' => ($item['amount_4'] ?? null),
+            ], [
+                'scent_id_1' => $item['scent_id_1'],
+                'amount_1' => $item['amount_1'],
+                'scent_id_2' => ($item['scent_id_2'] ?? null),
+                'amount_2' => ($item['amount_2'] ?? null),
+                'scent_id_3' => ($item['scent_id_3'] ?? null),
+                'amount_3' => ($item['amount_3'] ?? null),
+                'scent_id_4' => ($item['scent_id_4'] ?? null),
+                'amount_4' => ($item['amount_4'] ?? null),
+            ]);
+        }
     }
+
+    private function createSingles()
+    {
+        foreach (Scent::all() as $item) {
+            Combination::updateOrCreate([
+                'scent_id_1' => $item->id,
+                'scent_id_2' => null
+            ], [
+                'scent_id_1' => $item->id,
+                'amount_1' => 1
+            ]);
+        }
+    }
+
 }
