ab5912b304abaff742e1761f5be922d22a58e3fe
Mike Bros <mbros@roofmaxx.com>
2021-10-28 08:39:56 -0500

    table data is now being displayed on Jar and Wick pages

--- resources/views/jar/index.blade.php
+++ resources/views/jar/index.blade.php
@@ -4,10 +4,37 @@
 
     <div class="container">
         <div class="row justify-content-center">
-            <h1><i class="fas fa-glass-whiskey"></i> Jar</h1>
+            <h1><i class="fas fa-glass-whiskey"></i> Jar ({{count($jars)}})</h1>
         </div>
         <br>
 
+        @if(!empty($jars))
+            <table class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Size</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($jars as $jar)
+                    <tr>
+                        <td>{{$jar->id}}</td>
+                        <td><a href="{{$jar->order_link}}" target="_blank">{{$jar->name}}</a></td>
+                        <td>{{$jar->size}} oz</td>
+                        <td>{{$jar->quanity}}</td>
+                        <td>${{number_format($jar->price/100, 2)}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @else
+            <p>There are no jars...</p>
+        @endif
+    </div>
     </div>
 
 @endsection
--- resources/views/wick/index.blade.php
+++ resources/views/wick/index.blade.php
@@ -4,10 +4,38 @@
 
     <div class="container">
         <div class="row justify-content-center">
-            <h1><i class="fas fa-broom fa-rotate-180"></i> Wick</h1>
+            <h1><i class="fas fa-broom fa-rotate-180"></i> Wick ({{count($wicks)}})</h1>
         </div>
         <br>
 
+        @if(!empty($wicks))
+            <table class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Thickness</th>
+                    <th>Length</th>
+                    <th>Price</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($wicks as $wick)
+                    <tr>
+                        <td>{{$wick->id}}</td>
+                        <td><a href="{{$wick->order_link}}" target="_blank">{{$wick->name}}</a></td>
+                        <td>{{$wick->type}}</td>
+                        <td>{{$wick->thickness}} mm</td>
+                        <td>{{$wick->length}} ft</td>
+                        <td>${{number_format($wick->price/100, 2)}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @else
+            <p>There are no wicks...</p>
+        @endif
     </div>
 
 @endsection
